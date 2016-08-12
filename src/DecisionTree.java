186ee304756d989b6fa235f8c9ab10661262fbac
anilmodi2011 <anilmodi20112@gmail.com>
2016-08-13 01:59:47 +0530

    added tree and node classes
    It has basic getters and setters all other functions for traversing will
    be added later.

--- src/DecisionTree.java
+++ src/DecisionTree.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+
 public class DecisionTree {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
@@ -13,25 +14,119 @@ public class DecisionTree {
 		    int numberOfFeatures= Integer.parseInt(br.readLine());
 		    System.out.println("number of Features is: " + numberOfFeatures); 
 		    
-		    int du;
 		    String line;
 		    List<Instance> InstanceSet = new ArrayList<Instance>();
 		    while ((line = br.readLine()) != null) {
 		    	InstanceSet.add(new Instance(line));
-		    	du = InstanceSet.get(InstanceSet.size()-1).getClassLabel();
-		    	System.out.println("from the method "+ du);
-		    }
+		     }
 		    
 		    long seed = System.nanoTime();
 		    Collections.shuffle(InstanceSet, new Random(seed)); // randomised the instance set.
-		    List<Instance> TrainingSet ;
-		    List<Instance> TestSet;
+		    List<Instance> TrainingSet = new ArrayList<Instance>();
+		    for (int i = 0; i < 1000; i++) {
+		    	TrainingSet.add(InstanceSet.remove(i));
+			}
+		    
 		}
 		catch (Exception e) {
 			System.out.println("Error in opening file!");
 		}
 	}
 
+}
+
+//tree class to initiate tree
+class Tree {
+	private Node root;
+	//initializing a tree
+    public Tree() {
+        super();
+    }
+    public Node getRoot() {
+        return this.root;
+    }
+    //setting tree root
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+}
+
+// Node class to create tree node
+class Node {
+	//attribute list as integer
+	public List<Integer> attr;
+	//children list
+    public List<Node> children;
+    
+    public Node parent;
+    //Instance list
+    public List<Instance> inst;
+    
+    public Node() {
+        super();
+    }
+    //Initializing Node by giving List of Attribute and Instances
+    // we can set children and parent using given functions
+    public Node(List<Integer> attr,List<Instance> inst) {
+        this();
+        this.parent=null;
+        setAttr(attr);
+        setInst(inst);
+    }
+    
+    //to get children array list
+    public List<Node> getChildren() {
+        if (this.children == null) {
+            return new ArrayList<Node>();
+        }
+        return this.children;
+    }
+    //to get children arraylist of Nodes
+    public void setChildren(List<Node> children) {
+        this.children = children;
+    }
+    
+    public int getNumberOfChildren() {
+        if (children == null) {
+            return 0;
+        }
+        return children.size();
+    }
+    
+    //addChild to children array
+    public void addChild(Node child) {
+        if (children == null) {
+            children = new ArrayList<Node>();
+        }
+        children.add(child);
+    }
+   
+    //get attribute list for this node
+    public List<Integer> getData() {
+        return this.attr;
+    }
+    // set attribute list of integers
+    public void setAttr(List<Integer> attr) {
+        this.attr = attr;
+    }
+    // get Instances in current node
+    public List<Instance> getInst() {
+         return this.inst;
+    }
+    
+    //set instances in current node
+    public void setInst(List<Instance> inst) {
+         this.inst = inst;
+    }
+    
+    //setting parent node
+    public void setParent(Node par){
+    	this.parent=par;
+    } 
+    
+    public Node getParent(){
+    	return this.parent;
+    }
 }
 
 
@@ -41,7 +136,7 @@ class Instance{
 	Instance(String s){
 		try{
 			featureList = toIntArray(s);
-			System.out.println(classLabel);
+//			System.out.println(classLabel);
 		}
 		catch (Exception c){
 			System.out.println("error in instance");
