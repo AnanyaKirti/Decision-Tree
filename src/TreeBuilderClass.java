dfc841d05bd2bcb6d40f3d5a9bae3bb14d399ddd
Ananya Kirti <ananya.kirti@iitrpr.ac.in>
2016-08-14 03:20:32 +0530

    something
    something

--- src/TreeBuilderClass.java
+++ src/TreeBuilderClass.java
@@ -14,6 +14,7 @@ public abstract class TreeBuilderClass {
 		
 		// initialise the root node of the tree
 		Node rootNode = new Node();
+		BuildTreeHelper(rootNode, file.TrainingSet);
 		
 		return rootNode;
 	}
@@ -25,9 +26,30 @@ public abstract class TreeBuilderClass {
 	 * @param attributeAvailable The attributes that have not yet been used
 	 * @param instances	The instances available at the node.
 	 */
-	public void BuildTreeHelper(Node node, List<Integer> attributeAvailable, List<Instance> instances){
+	public static void BuildTreeHelper(Node node, List<Instance> instances){// , List<Integer> attributeAvailable,){
+		int positiveValues = 0;
+		int negativeValues = 0;
+		int totalValues = instances.size();
+		
+		for (Instance instance : instances) {
+			if(instance.getClassLabel() == 0){
+				positiveValues++;
+			}
+			else{
+				negativeValues++;
+			}
+		}
+//		System.out.println(positiveValues);
+//		System.out.println(negativeValues);
+//		System.out.println(Math.log(positiveValues));
+//		System.out.println(Math.log(totalValues));
+		
+		float entropy = (float) (-1*(positiveValues/totalValues * (Math.log(positiveValues) - Math.log(totalValues)))
+				+  (negativeValues/totalValues * (Math.log(negativeValues) - Math.log(totalValues))  )) ;
+		System.out.println(entropy);
 		
 	}
+	
 	
 	public int getBestAttribute(){
 		int bestAttribute = -1;
