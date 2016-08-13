8132104c86acb6ffe2392946d3d18f8142599674
Ananya Kirti <ananya.kirti@iitrpr.ac.in>
2016-08-14 02:15:22 +0530

    removed median
    removed median, and also getBestAttribute Function.

--- src/DecisionTree.java
+++ src/DecisionTree.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.util.*;
 
-import sun.reflect.ReflectionFactory.GetReflectionFactoryAction;
-
 
 public class DecisionTree {
 
@@ -37,42 +35,14 @@ public class DecisionTree {
 		    //randomised the instance set.
 		    long seed = System.nanoTime();
 		    Collections.shuffle(InstanceSet, new Random(seed)); 
-
-		    
-		    // helper to get the median of the features.
-		    List<List<Integer>> featureRange = new ArrayList<List<Integer>>();
-		    for (int i = 0; i < numberOfFeatures-1; i++) {
-				featureRange.add(i, new ArrayList<Integer>());
-			}
-
 		    
 		    // select the first 1000 instances as the Training set, the rest become the test set.
 		    List<Instance> TrainingSet = new ArrayList<Instance>();
 		    for (int i = 0; i < 1000; i++) {
 		    	// add the instance to the TrainingSet, and delete it from the Instance Set.
 		    	TrainingSet.add(InstanceSet.remove(0));
-		    	for (int j = 0; j < numberOfFeatures-1; j++) {
-		    		// Populate the featureRange.
-		    		featureRange.get(j).add(TrainingSet.get(i).getFeatureValue(j));
-				}
 			}
-		    
-		    // we get the median feature, to decide where to split the data.
-		    int[] medianFeature= new int[numberOfFeatures -1];
-		    for (int i = 0; i < numberOfFeatures-1 ; i++) {
-				Collections.sort(featureRange.get(i));
-				medianFeature[i] = featureRange.get(i).get(1000/2) + 1;
-				if (featureRange.get(i).get(0) == 0 && featureRange.get(i).get(numberOfFeatures - 1) == 1) {
-					medianFeature[i] = 1;
-				}
-			}
-
-		    
-		    
-			// Now we have generated the test data, and training data.
-		    
 			
-		    
 		    
 		}
 		catch (Exception e) {
@@ -88,39 +58,3 @@ public class DecisionTree {
 	
 	
 	
-
-	
-	
-	/**
-	 * Method to extract the best feature
-	 * @param attr	The list of available attributes.
-	 * @param inst	The list of the instances available at the node.
-	 * @param medianFeature the median of the features, used to split the data.
-	 * @return The index of the best feature, the target feature.
-	 */
-//	@SuppressWarnings("unused")
-//	private static int getBestFeature( List<Integer> attr, List<Instance> inst, int[] medianFeature, float entropy){
-//		
-//		// TODO finish the function
-//		
-//		float informationGain = 0f;
-//		float bestInformationGain = 0f;
-//		float entropy = 1f;
-//		float entropyLeft = 1f;
-//		float entropyRight = 1f;
-//		
-//		int targetAtribute = -1;
-//		for (int index = 0; index < attr.size(); index++) {
-//			if (attr.get(index)== 1) {
-//				informationGain = 0;
-//				for (Instance instance : inst) {
-//					instance.getFeatureValue(index);
-//				}
-//			}
-//		}
-//
-//		
-//		return targetAtribute;
-//	}
-//
-//}
