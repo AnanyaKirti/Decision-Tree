17498130446c5bc1c8ce2182e4865dcbf49eb89d
Ananya Kirti <ananya.kirti@iitrpr.ac.in>
2016-08-21 18:54:57 +0530

    added forest info
    forest sys out changed to what is required

--- src/Test4.java
+++ src/Test4.java
@@ -8,15 +8,15 @@ public class Test4 {
 	public static void Start(String fileName) {
 		File file = new File(fileName);
 		List<Node> forest = new ArrayList<Node>();
-
-		int numberOfTrees= 100;
-		for (int i = 0; i < numberOfTrees; i++) {
-			forest.add(i, RandomTreeBuilder.BuildTree(file));
-		}
-
+		
 		float accuracy;
-		accuracy = RandomClassifier.TreeTester(forest, file);
-
-		System.out.println("Tree Classification Accuracy: " + accuracy);
+		int numberOfTrees= 100;
+		for(int j = 0; j < numberOfTrees; j++){
+			for (int i = 0; i < j; i++) {
+				forest.add(i, RandomTreeBuilder.BuildTree(file));
+			}
+			accuracy = RandomClassifier.TreeTester(forest, file);
+			System.out.println("Forest" + j +" Classification Accuracy: " + accuracy);
+		}
 	}
 }
--- src/TreeTesterClass.java
+++ src/TreeTesterClass.java
@@ -65,7 +65,6 @@ public abstract class TreeTesterClass {
 		} else {
 			return Majority(node);
 		}
-
 	}
 
 	/**
